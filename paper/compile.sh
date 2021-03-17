2f57a04077acf0e640ddbd4867e9853e42b0d843
dustydins <arrandinsmore@protonmail.com>
2021-03-17 12:49:53 +0000

    first commit

---
+++ paper/compile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pdflatex /home/ald/Documents/Heriot-Watt/MSCPROJ/thesis/main.tex
+bibtex /home/ald/Documents/Heriot-Watt/MSCPROJ/thesis/main
+pdflatex /home/ald/Documents/Heriot-Watt/MSCPROJ/thesis/main.tex
+pdflatex /home/ald/Documents/Heriot-Watt/MSCPROJ/thesis/main.tex
